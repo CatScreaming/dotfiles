1464abed6c83c64d416624306979ff5c383887bd
CatScreaming <david-lindsay@keemail.me>
2023-01-08 16:03:21 +0100

    Powershell refactor

--- .dotfiles/install.ps1
+++ .dotfiles/install.ps1
@@ -2,7 +2,7 @@
 robocopy ../.config/nvim/ $HOME\AppData\Local\nvim\ /e
 
 # Copy wt configuration
-robocopy win/ $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ settings.json
+robocopy windows/ $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ settings.json
 
 # Copy lf configuration
-robocopy win/lf/ $HOME\AppData\Local\lf\
+robocopy windows/lf/ $HOME\AppData\Local\lf\
